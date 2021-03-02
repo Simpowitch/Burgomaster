284ec87d4d4bc8f56a8de87fc887f708b9653270
Simon Voss <55984457+Simwpowitch@users.noreply.github.com>
2021-03-02 17:18:57 +0100

    path creation start

---
+++ Assets/Scripts/Node/Editor/PathEditor.cs
@@ -0,0 +1,178 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathCreator))]
+public class PathEditor : Editor
+{
+    PathCreator creator;
+    Path Path => creator.path;
+    
+    const float SEGMENTSELECTDISTANCETHRESHOLD = 0.1f;
+    int selectedSegmentIndex = -1;
+
+    private void OnEnable()
+    {
+        creator = (PathCreator)target;
+        if (creator.path == null)
+        {
+            creator.CreatePath();
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        EditorGUI.BeginChangeCheck();
+
+        if (GUILayout.Button("Create New"))
+        {
+            Undo.RecordObject(creator, "Create new");
+            creator.CreatePath();
+        }
+
+        bool isClosed = GUILayout.Toggle(Path.IsClosed, "Closed");
+        if (isClosed != Path.IsClosed)
+        {
+            Undo.RecordObject(creator, "Toggle closed");
+            Path.IsClosed = isClosed;
+        }
+
+        bool autoSetControlPoints = GUILayout.Toggle(Path.AutoSetControlPoints, "Auto Set Control Points");
+        if (autoSetControlPoints != Path.AutoSetControlPoints)
+        {
+            Undo.RecordObject(creator, "Toggle auto set controls");
+            Path.AutoSetControlPoints = autoSetControlPoints;
+        }
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            SceneView.RepaintAll();
+        }
+    }
+
+    private void OnSceneGUI()
+    {
+        Input();
+        Draw();
+
+        if (creator.audoUpdate && Event.current.type == EventType.Repaint)
+        {
+            creator.UpdateRoad();
+        }
+    }
+
+    void Input()
+    {
+        Event guiEvent = Event.current;
+        Vector2 mousePos = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition).origin;
+
+        if (guiEvent.type == EventType.MouseMove)
+        {
+            float minDstToSegment = SEGMENTSELECTDISTANCETHRESHOLD;
+            int newSelectedSegmentIndex = -1;
+
+            //Find closest segment
+            for (int i = 0; i < Path.NumberOfSegments; i++)
+            {
+                Vector2[] points = Path.GetPointsInSegment(i);
+                float dst = HandleUtility.DistancePointBezier(mousePos, points[0], points[3], points[1], points[2]);
+
+                if (dst < minDstToSegment)
+                {
+                    minDstToSegment = dst;
+                    newSelectedSegmentIndex = i;
+                }
+            }
+
+            if (newSelectedSegmentIndex != selectedSegmentIndex)
+            {
+                selectedSegmentIndex = newSelectedSegmentIndex;
+                HandleUtility.Repaint();
+            }
+        }
+
+        //Leftclick + Shift
+        if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0 && guiEvent.shift)
+        {
+            if (selectedSegmentIndex != -1)
+            {
+                Undo.RecordObject(creator, "Split Segment");
+                Path.SplitSegment(mousePos, selectedSegmentIndex);
+            }
+            else if (!Path.IsClosed)
+            {
+                Undo.RecordObject(creator, "Add Segment");
+                Path.AddSegment(mousePos);
+            }
+        }
+
+        //Rightclick
+        if (guiEvent.type == EventType.MouseDown && guiEvent.button == 1)
+        {
+            float minDstToAnchor = creator.anchorDiameter * 0.5f;
+            int closestAnchorIndex = -1;
+
+            //Find closest point to the mouse
+            for (int i = 0; i < Path.NumberOfPoints; i += 3)
+            {
+                float dst = Vector2.Distance(mousePos, Path[i]);
+
+                if (dst < minDstToAnchor)
+                {
+                    minDstToAnchor = dst;
+                    closestAnchorIndex = i;
+                }
+            }
+
+            if (closestAnchorIndex != -1)
+            {
+                Undo.RecordObject(creator, "Remove Segment");
+                Path.DeleteSegment(closestAnchorIndex);
+            }
+        }
+
+        HandleUtility.AddDefaultControl(0);
+    }
+
+    void Draw()
+    {
+        Handles.color = Color.black;
+        for (int i = 0; i < Path.NumberOfSegments; i++)
+        {
+            Vector2[] points = Path.GetPointsInSegment(i);
+
+            if (creator.displayControlPoints)
+            {
+                Handles.DrawLine(points[1], points[0]);
+                Handles.DrawLine(points[2], points[3]);
+            }
+
+            Color segmentColor = i == selectedSegmentIndex && Event.current.shift ? creator.selectedSegmentColor : creator.segmentColor;
+
+            Handles.DrawBezier(points[0], points[3], points[1], points[2], segmentColor, null, 2f);
+        }
+
+        for (int i = 0; i < Path.NumberOfPoints; i++)
+        {
+            bool isAnchorPoint = i % 3 == 0;
+
+            if (isAnchorPoint || creator.displayControlPoints)
+            {
+                Handles.color = isAnchorPoint ? creator.anchorColor : creator.controlColor;
+                float handleSize = isAnchorPoint ? creator.anchorDiameter : creator.controlDiameter;
+
+                Vector2 newPos = Handles.FreeMoveHandle(Path[i], Quaternion.identity, handleSize, Vector2.zero, Handles.CylinderHandleCap);
+                if (Path[i] != newPos)
+                {
+                    Undo.RecordObject(creator, "Move point");
+                    Path.MovePoint(i, newPos);
+                }
+            }
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Node/PathCreator.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class PathCreator : MonoBehaviour
+{
+    [HideInInspector]
+    public Path path;
+
+    public float pathWidth = 2f;
+    [Range(0.5f, 1.5f)]
+    public float spacing = 1f;
+
+    [Header("Gizmos and Editor")]
+    public Color anchorColor = Color.red;
+    public Color controlColor = Color.white;
+    public Color segmentColor = Color.green;
+    public Color selectedSegmentColor = Color.yellow;
+
+    public float anchorDiameter = 0.1f;
+    public float controlDiameter = 0.075f;
+    public bool displayControlPoints = true;
+    public bool audoUpdate;
+
+    private void Reset()
+    {
+        CreatePath();
+    }
+
+    public void CreatePath()
+    {
+        path = new Path(transform.position);
+    }
+
+    public void UpdateRoad()
+    {
+        Vector2[] points = path.CalculateEvenlySpacedPoints(spacing);
+        GetComponent<MeshFilter>().mesh = CreatePathMesh(points, path.IsClosed);
+    }
+
+    Mesh CreatePathMesh(Vector2[] points, bool isClosed)
+    {
+        Vector3[] verts = new Vector3[points.Length * 2];
+        Vector2[] uvs = new Vector2[verts.Length];
+
+        int numTris = 2 * (points.Length - 1) + (isClosed ? 2 : 0);
+
+        int[] tris = new int[numTris * 3];
+        int vertIndex = 0;
+        int triIndex = 0;
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            Vector2 forward = Vector2.zero;
+            if (i < points.Length - 1 || isClosed)
+            {
+                forward += points[(i + 1) % points.Length] - points[i];
+            }
+            if (i > 0 || isClosed)
+            {
+                forward += points[i] - points[(i - 1 + points.Length)% points.Length];
+            }
+            forward.Normalize();
+
+            Vector2 left = new Vector2(-forward.y, forward.x);
+
+            verts[vertIndex] = points[i] + left * pathWidth * 0.5f;
+            verts[vertIndex + 1] = points[i] - left * pathWidth * 0.5f;
+
+            //Uvs
+            float completionPercent = i / (float)(points.Length - 1);
+            float v = 1 - Mathf.Abs(2 * completionPercent - 1);
+            uvs[vertIndex] = new Vector2(0, v);
+            uvs[vertIndex + 1] = new Vector2(1, v);
+
+            if (i < points.Length - 1 || isClosed)
+            {
+                tris[triIndex] = vertIndex;
+                tris[triIndex + 1] = (vertIndex + 2) % verts.Length;
+                tris[triIndex + 2] = vertIndex + 1;
+
+                tris[triIndex + 3] = vertIndex + 1;
+                tris[triIndex + 4] = (vertIndex + 2) % verts.Length;
+                tris[triIndex + 5] = (vertIndex + 3) % verts.Length;
+            }
+
+            vertIndex += 2;
+            triIndex += 6;
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = verts;
+        mesh.triangles = tris;
+        mesh.uv = uvs;
+
+        return mesh;
+    }
+}
