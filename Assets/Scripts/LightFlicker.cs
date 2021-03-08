789a6d9420dce113ab4adbd227a2c63b6d74f5d6
Simon Voss <55984457+Simpowitch@users.noreply.github.com>
2021-03-08 16:14:07 +0100

    Torchlight

--- Assets/Scripts/LightFlicker.cs
+++ Assets/Scripts/LightFlicker.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
+[RequireComponent(typeof(Light2D))]
 public class LightFlicker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Light2D source;
+
+    [Range(0f, 2f)]
+    public float minIntensity = 0.1f, maxIntensity = 2f;
+    [Range(0f, 1f)]
+    public float minTimeChange = 0.1f, maxTimeChange = 0.75f;
+
+    private void Awake()
     {
-        
+        source = GetComponent<Light2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(Lerp());
+    }
+
+    IEnumerator Lerp()
+    {
+        float startIntensity = source.intensity;
+        float t = 0;
+        float deltaTime = 0;
+
+        float targetIntensity = Random.Range(minIntensity, maxIntensity);
+        float changeTime = Random.Range(minTimeChange, maxTimeChange);
+
+        while(t < 1)
+        {
+            deltaTime += Time.deltaTime;
+            t = deltaTime / changeTime;
+
+            source.intensity = Mathf.Lerp(startIntensity, targetIntensity, t);
+            yield return null;
+        }
+
+        source.intensity = targetIntensity;
+        yield return null;
+
+        StartCoroutine(Lerp());
     }
 }
