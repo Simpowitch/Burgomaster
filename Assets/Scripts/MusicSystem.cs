c014a98b96ddd548add9f2e8aabe22a8ce64a655
Simpowitch <woss.simon@gmail.com>
2021-03-15 17:42:35 +0100

    music player

---
+++ Assets/Scripts/MusicSystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MusicSystem : MonoBehaviour
+{
+    public AudioSource audioPlayer;
+    public float minPauseTime = 30f, maxPauseTime = 180f;
+    public AudioClip[] scores;
+    int scoreIndex;
+    bool replayingAfterDuration = false;
+
+    private void Start()
+    {
+        scoreIndex = Random.Range(0, scores.Length);
+        audioPlayer.clip = scores[scoreIndex];
+        audioPlayer.Play();
+        audioPlayer.loop = false;
+    }
+
+    private void Update()
+    {
+        if (!audioPlayer.isPlaying && !replayingAfterDuration)
+            StartCoroutine(ReplayMusicAfterDuration(Random.Range(minPauseTime, maxPauseTime)));
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            StopAllCoroutines();
+            PlayNextSong();
+            replayingAfterDuration = false;
+        }
+    }
+
+    IEnumerator ReplayMusicAfterDuration(float duration)
+    {
+        replayingAfterDuration = true;
+        Debug.Log($"Pausing music and replaying in: { duration} seconds");
+        yield return new WaitForSeconds(duration);
+        PlayNextSong();
+        replayingAfterDuration = false;
+    }
+
+    void PlayNextSong()
+    {
+        scoreIndex++;
+        scoreIndex = scoreIndex % scores.Length;
+        Debug.Log("Score index:" + scoreIndex);
+        audioPlayer.clip = scores[scoreIndex];
+        audioPlayer.Play();
+    }
+}
